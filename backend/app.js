2e2f5cc8e0fa9ab496524e644912c36616f96f95
galaxy999280 <galaxy990928@naver.com>
2021-06-28 21:00:50 +0900

    build

--- backend/app.js
+++ backend/app.js
@@ -4,10 +4,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-app.get('/', (req, res) => {
-    res.send('Hello Geeks');
-});
-
 app.listen(4000, () => {
     console.log(`Server listening on http://localhost:4000`)
 })
